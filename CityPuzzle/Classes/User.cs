6e3588cbf60fd2b00f97ba2c8f59fa203ae6b85c
Rokas <Rokas@LAPTOP-5NAFTNSP>
2021-09-28 21:36:33 +0300

    Encrypt and decrypt password

--- CityPuzzle/Classes/User.cs
+++ CityPuzzle/Classes/User.cs
@@ -1,6 +1,8 @@
 ﻿using SQLite;
 using System;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CityPuzzle.Classes
 {
@@ -53,6 +55,72 @@ namespace CityPuzzle.Classes
 
             };
             return true;
+        }
+        public static void EncryptPass(string pass)
+        {
+            var text = "opa";
+
+            var encryptedText = EncryptPlainTextToCipherText(text);
+            var decryptedText = DecryptCipherTextToPlainText(encryptedText);
+
+
+            Console.WriteLine("Passed Text = " + text);
+            Console.WriteLine("EncryptedText = " + encryptedText);
+            Console.WriteLine("DecryptedText = " + decryptedText);
+            Console.ReadLine();
+        }
+        private const string SecurityKey = "ComplexKeyHere_12121";
+        public static string EncryptPlainTextToCipherText(string PlainText)
+        {
+           
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+           
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            
+            objTripleDESCryptoService.Key = securityKeyArray;
+            
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+            
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+            objTripleDESCryptoService.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        
+        public static string DecryptCipherTextToPlainText(string CipherText)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(CipherText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+
+           
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            
+            objTripleDESCryptoService.Key = securityKeyArray;
+            
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+           
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
+            
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            objTripleDESCryptoService.Clear();
+
+            
+            return UTF8Encoding.UTF8.GetString(resultArray);
         }
     }
 
