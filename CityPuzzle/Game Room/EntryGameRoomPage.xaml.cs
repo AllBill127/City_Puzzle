3a9adc5a6a0706b004a2a334f87492007b563558
Justuxs <pegasas@yahoo.com>
2021-10-18 19:14:20 +0300

    Bandymas grup join- entrygameroompage

--- CityPuzzle/Game Room/EntryGameRoomPage.xaml.cs
+++ CityPuzzle/Game Room/EntryGameRoomPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace CityPuzzle
 {
@@ -13,20 +14,33 @@ namespace CityPuzzle
     {
         public String EntryRoomID;
         public List<Room> AllRooms = new List<Room>();
+        public List<User> AllUsers= new List<User>();
         public Room CurrentRoom;
         public EntryGameRoomPage()
         {
             InitializeComponent();
             getRoomId();
-
-
         }
+
+        public void ShowInfo()
+        {
+        XElement grupedList = new XElement("GameOwners",
+        from user in AllUsers
+        join Room in AllRooms on user.UserName equals Room.Owner into ownerName
+        select new XElement("Owner",
+            new XAttribute("FirstName", person.FirstName),
+            new XAttribute("LastName", person.LastName),
+            from subpet in ownerName
+            select new XElement("Pet", subpet.Name)));
+        }
+
         async void getRoomId()
         {
             string message = await DisplayPromptAsync("Dalyvavimas dalyviu žaidime", "Ivesk Room ID?");
             try
             {
                 CurrentRoom = AllRooms.SingleOrDefault(x => x.ID.ToLower().Equals(message.ToLower()));
+                ShowInfo();
 
             }
             catch
@@ -42,7 +56,13 @@ namespace CityPuzzle
             {
                 conn.CreateTable<Room>();
                 AllRooms = conn.Table<Room>().ToList();
-            }}
+            }
+            using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
+            {
+                conn.CreateTable<User>();
+                AllUsers = conn.Table<User>().ToList();
+            }
+        }
 
     }
 }
