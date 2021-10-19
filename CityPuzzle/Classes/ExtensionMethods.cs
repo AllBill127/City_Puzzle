220e3433d626059d71728d879b9884b5486b02a2
crmsnwl <crimsonowl@yahoo.com>
2021-10-19 19:43:06 +0300

    Create Top10 method, use IEquatable

--- CityPuzzle/Classes/ExtensionMethods.cs
+++ CityPuzzle/Classes/ExtensionMethods.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static CityPuzzle.Classes.Structs;
 
 namespace CityPuzzle.Classes
 {
     public static class ExtensionMethods
     {
         // Extension method for User class finding top 10 users by completed quest count
-        public static List<User> Top10(this List<User> users)
+        public static List<UserInfo> Top10(this List<User> users)
         {
             users.Sort(new PointsComparer());
 
-            List<User> top10 = new List<User>();
-            for (int i = 0; i < 10; ++i)
-            {
-                top10.Add(users[i]);
-            }
+            List<UserInfo> TopUsers = new List<UserInfo>();
 
-            return top10;
+            int i = 0; //iteruoja pro listus
+            int j = 1; //nusako rezultato indexa 
+            UserInfo temp = new UserInfo { };
+
+            while (j <= 10 && users[i] != null)
+            {  
+                temp.name = users[i].Name;
+                temp.score = users[i].QuestsCompleted.Count;
+                temp.index = j;
+                
+                TopUsers[i] = temp;
+                
+                if (users[i + 1].Equals(users[i]) == false)
+                    ++j;
+
+                ++i;
+            }
+            return TopUsers;
         }
     }
 }
