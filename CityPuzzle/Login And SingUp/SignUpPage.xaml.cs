0c8d85720001a1287a6a827cff9ad9d93066957d
Aleksandras <aleksandrasbilevicius@gmail.com>
2021-11-09 17:22:09 +0200

    Fix commented problems

--- CityPuzzle/Login And SingUp/SignUpPage.xaml.cs
+++ CityPuzzle/Login And SingUp/SignUpPage.xaml.cs
@@ -1,12 +1,7 @@
 ﻿using CityPuzzle.Classes;
-using SQLite;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using System.Collections.Generic;
 
 namespace CityPuzzle
@@ -29,25 +24,25 @@ namespace CityPuzzle
 
             foreach (Entry field in fields)
             {
-                if (String.IsNullOrWhiteSpace(field.Text))
+                if (string.IsNullOrWhiteSpace(field.Text))
                     throw new EmptyInputdException(field.Placeholder, field);
 
-                // field.Placeholder is defined in SignUpPage.xaml Entity propertie for text fields
+                // field.Placeholder is defined in SignUpPage.xaml Entry object properties for text input fields
                 switch (field.Placeholder)
                 {
                     case "Vartotojo vardas":
                         if (!validateUsername(field.Text)) 
-                            throw new BadInputdException("nuo 6 iki 12 ilgio", field.Placeholder, field);
+                            throw new BadInputdException("Vartotojo vardas turi būti 6 - 12 ženklų ilgio", field.Placeholder, field);
                         else if (!User.CheckUser(field.Text)) 
-                            throw new BadInputdException("Naudotojas su tokiu prisijungimo vardu užimtas.Pakeiskite prisijungimo vardą ir bandykite dar kartą.", field);
+                            throw new BadInputdException("Vartotojo vardas užimtas", field);
                         break;
                     case "Slaptazodis":
                         if (!validatePassword(field.Text)) 
-                            throw new BadInputdException("Password must be atleast 8 to 15 characters and contains atleast one Upper case and numbers.", field);
+                            throw new BadInputdException("Slaptažodis turi būti 8 - 15 ženklų ilgio ir panaudoti bent vieną didžiają raidę ir skaičių", field);
                         break;
                     case "Pastas":
                         if (!validateEmail(field.Text)) 
-                            throw new BadInputdException("Invalid Email Address!", field);
+                            throw new BadInputdException("Neteisingas el. pašto adresas", field);
                         break;
                     default:
                         break;
@@ -68,8 +63,8 @@ namespace CityPuzzle
                 fields.Add(lastnameEntry);
                 fields.Add(emailEntry);
                 Validation(fields);
-                User user;
 
+                User user;
                 if (distEntry.Text != null)
                 {
                     user = CreateUser(name: nameEntry.Text, userName: usernameEntry.Text, lastName: lastnameEntry.Text, password: User.PassToHash(passEntry.Text), email: emailEntry.Text, maxDist: double.Parse(distEntry.Text));
@@ -88,14 +83,14 @@ namespace CityPuzzle
                 ErrorAllert(exception.Message);
                 exception.Field.BackgroundColor = Color.Orange;
                 exception.Field.TextColor = Color.Red;
-                exception.Field.Text = "Neteisingai įvedėt  šį lauką";
+                exception.Field.Text = "Neteisingai įvedėt šį lauką";
             }
             catch (EmptyInputdException exception)
             {
 
                 ErrorAllert(exception.Message);
                 exception.Field.BackgroundColor = Color.Red;
-                exception.Field.Text = "Prašom užpildyti šį lauką ";
+                exception.Field.Text = "Prašom užpildyti šį lauką";
 
             }
 
