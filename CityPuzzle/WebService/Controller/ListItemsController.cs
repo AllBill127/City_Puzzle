df099818cadf9525e63aed1a91f1c449f826454e
Rokas <Rokas@LAPTOP-5NAFTNSP>
2021-11-23 21:36:43 +0200

    Added Controller to web service

---
+++ CityPuzzle/WebService/Controller/ListItemsController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+
+namespace CityPuzzle.WebService.Controller
+{
+    public class ListItemsController
+    {
+        // GET api/<controller>
+        public IEnumerable<CustomListItem> Get()
+        {
+            return _listItems;
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody] CustomListItem model)
+        {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var maxId = 0;
+            if (_listItems.Count > 0)
+            {
+                maxId = _listItems.Max(x => x.Id);
+            }
+            model.Id = maxId + 1;
+            _listItems.Add(model);
+            return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage Put(int id, [FromBody] CustomListItem model)
+        {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                // Update *all* of the item's properties
+                item.Text = model.Text;
+
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _listItems.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+    }
+}
