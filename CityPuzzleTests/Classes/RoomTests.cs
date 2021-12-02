c288ac24608f29d68f66f88ace15b633000b74f0
crmsnwl <crimsonowl@yahoo.com>
2021-12-02 11:11:06 +0200

    Create Room tests

---
+++ CityPuzzleTests/Classes/RoomTests.cs
@@ -0,0 +1,46 @@
+﻿using CityPuzzle.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CityPuzzle.Classes.Tests
+{
+    public class RoomTests
+    {
+        [Theory]
+        [MemberData(nameof(GetUsers))]
+        public void setParticipantsTestPass(User user)
+        {
+            Room room = new Room() { ID = "", Owner = 0, RoomSize = 0};
+
+            room.setParticipants(user);
+
+            Assert.Contains(1478, room.ParticipantIDs);
+            Assert.Contains(1410, room.ParticipantIDs);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUsers))]
+        public void setParticipantsTestFail(User user)
+        {
+            Room room = new Room() { ID = "", Owner = 0, RoomSize = 0 };
+
+            room.setParticipants(user);
+
+            Assert.DoesNotContain(1889, room.ParticipantIDs);
+        }
+
+        public static IEnumerable<object[]> GetUsers
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new User(new UserVerifier()) { ID = 1478 } },
+                    new object[] { new User(new UserVerifier()) { ID = 1410 } }
+                };
+            }
+        }
+    }
+}
