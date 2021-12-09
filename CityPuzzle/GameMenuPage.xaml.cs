8e61dfba1b9f008167ce31f87269b9e906e6fa64
Justuxs <pegasas@yahoo.com>
2021-12-09 14:21:09 +0200

    only for test

--- CityPuzzle/GameMenuPage.xaml.cs
+++ CityPuzzle/GameMenuPage.xaml.cs
@@ -5,7 +5,10 @@ using Xamarin.Forms.Xaml;
 using System.Net;
 using CityPuzzle.Classes;
 using CityPuzzle.Game_Room.Join_GameRoom;
-
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+using CityPuzzle.Rest_Services.Client;
 
 namespace CityPuzzle
 {
@@ -45,6 +48,25 @@ namespace CityPuzzle
         private void Settings_Clicked(object sender, EventArgs e)
         {
             //Navigation.PushAsync(new SettingsPage());
+        }
+        
+        private void test_Clicked(object sender, EventArgs e)
+        {
+            Puzzle puzzle = new Puzzle() { Name = "Test_Puzzle", About = "Test_Puzzle", ImgAdress = "Test_Puzzle", Latitude = 55.00, Longitude = 100, Quest = "Test_Puzzle" };
+            APICommands WebServices = new APICommands("http://10.0.2.2:5000/api/");
+            puzzle.ChangeService(WebServices);
+            Thread save = new Thread(() => puzzle.Save());
+            save.Start();
+            save.Join();
+            while (puzzle.ID == 0)
+                Thread.Sleep(100);
+            Task<List<Puzzle>> obTask = Task.Run(() => WebServices.GetPuzzles());
+            obTask.Wait();
+            List<Puzzle> puzles = obTask.Result;
+            Console.WriteLine("id: "+puzzle.ID);
+            Console.WriteLine((puzles.Any(rt => rt.ID == puzzle.ID)));
+
+
         }
     }
 
